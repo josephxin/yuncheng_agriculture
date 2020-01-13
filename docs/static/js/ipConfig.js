249f3e43c8486fa48c121b02add30ede2e6010b9
joseph_xin <54150537@qq.com>
2020-01-13 11:51:56 +0800

    2020-1-13 6

---
+++ docs/static/js/ipConfig.js
@@ -0,0 +1,15 @@
+/* eolinker地址 */
+window.BASEURL_01 = "https://result.eolinker.com/dLTGc7245280794476d5b21f1fe2709a774d6e19ba66574?uri=";
+
+/* 测试环境 */
+//window.BASEURL_01 = "http://202.106.10.250:48002"; // 外网地址
+//window.BASEURL_01 = "http://192.168.200.34:8089"; // 内网地址
+//window.BASEURL_01 = ".";// 部署地址
+
+//window.BASEURL_login = "http://192.168.200.34:8082"; // 登录内网地址
+window.BASEURL_login = "https://result.eolinker.com/dLTGc7245280794476d5b21f1fe2709a774d6e19ba66574?uri=";
+
+//window.BASEURL_03 = "http://202.106.10.250:48002"; // 前端项目地址（部署时用不到，本地调试时能用得到，为了显示图片）
+window.BASEURL_03 = "http://192.168.200.34:8089"; // 前端项目地址（部署时用不到，本地调试时能用得到，为了显示图片）
+
+window.debugging = true;
